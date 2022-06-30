34107ebf9d79f68f68931b234b1ea624854ff684
TorobaevSher <108340083+TorobaevSher@users.noreply.github.com>
2022-07-01 03:38:20 +0600

    lesson 1. 2 задача

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает
+// два числа и выдает, какое число больше,
+// а кокое меньшее.
+
+Console.WriteLine("Введите первое число");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int second = Convert.ToInt32(Console.ReadLine());
+
+ if(first > second)Console.WriteLine(first);   
+else Console.WriteLine(second);
